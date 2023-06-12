cd35203032a2113df0e09b9238891a3d26b34f64
Parth Patel <parthkumar111patel@gmail.com>
2023-06-12 19:10:50 +0530

    v2.0.0 - ToDoList

--- app.js
+++ app.js
@@ -1,55 +1,72 @@
+//jshint esversion:6
+
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
-// console.log(date.getDay());
+const mongoose = require("mongoose");
+main().catch(err => console.log(err));
 
 const app = express();
 
-const items = ["Buy Food","Cook Food","Eat Food"];
-const workItems = [];
-// in const array -> it is possible to push item in array
-//                -> it throw an error on asining (i.e. myArr[0] = "Zero")
-
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.get("/",function(req,res){
-    let day = date.getDate(); // or date.getDay();
 
-    res.render('list', {listTitle: day,newListItem : items});
-});
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/todolistDB');
 
-app.post("/",function(req,res){
-    let item = req.body.newItem;
+    const itemSchema = new mongoose.Schema({
+        name: String
+    })
+        
+    const Item = mongoose.model("Item",itemSchema);
+        
+    const item1 = new Item({
+        name: "Welcome to your to do list."
+    });
+        
+    const item2 = new Item({
+        name: "Hit the + button to add new item."
+    });
+        
+    const item3 = new Item({
+        name: "<--Hit this to delete this item."
+    });
+        
+    const defaultItems = [item1, item2, item3];
+    let foundItems = await Item.find({});
+    if(foundItems.length === 0){
+        Item.insertMany(defaultItems);
+   }
 
-    // console.log(req.body); 
-    if(req.body.list === "Work"){
-        workItems.push(item);
-        res.redirect("/work");
-    }
-    else{
-        items.push(item);
+    app.get("/", function(req, res) {
+    
+        res.render("list", {listTitle: "Today", newListItems: foundItems});
+        
+    });
+
+    app.post("/", function(req, res){
+
+        const itemName = req.body.newItem;
+        const item = new Item({
+            name: itemName
+        });
+        item.save();
+        
         res.redirect("/");
-    }
-})
+    });
 
-// Work List
-app.get("/work",function(req,res){
-    res.render('list',{listTitle: "Work List",newListItem : workItems});
-});
+    app.get("/work", function(req,res){
+        res.render("list", {listTitle: "Work List", newListItems: workItems});
+    });
 
-app.post("/work",function(req,res){
-    let item = req.body.newItem;
-    workItems.push(item);
-    res.redirect("/work");
-});
+    app.get("/about", function(req, res){
+        res.render("about");
+    });
 
-// about
-app.get("/about",function(req,res){
-    res.render('about');
-});
+    app.listen(3000, function() {
+        console.log("Server started on port 3000");
+    });
 
-app.listen(3000, function(){
-    console.log("Server started on port 3000");
-});
+}
